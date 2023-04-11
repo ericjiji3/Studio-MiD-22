1d06be016c7306d21f58612dd6242c49895dc9b4
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2023-04-11 00:21:00 -0500

    added loader

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,8 +7,16 @@ import '@/styles/carousel.css';
 import '@/styles/about.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import {useEffect} from 'react';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+            const loader = document.getElementById('globalLoader');
+        if (loader)
+            loader.style.display = 'none';
+    }
+}, []);
   return (
     <>
       <Header/>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,22 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import loader from "./loader.js";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <head>
+          <style>
+              {loader}
+          </style>
+      </head>
       <body>
+        <div id={'globalLoader'}>
+              <div className="loader">
+                <div/>
+                <div/>
+            </div>
+        </div>
         <Main />
         <NextScript />
       </body>
