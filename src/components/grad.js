5faca72cbcdde73cff59e00e60a49fdcfbaeb5b7
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2023-04-24 15:20:39 -0500

    deploying pt5?

--- src/components/grad.js
+++ src/components/grad.js
@@ -1,87 +1,109 @@
+import grad1 from '../../public/images/grad/grad-1.jpg';
+import grad2 from'../../public/images/grad/grad-2.jpg';
+import grad3 from'../../public/images/grad/grad-3.jpg';
+import grad4 from'../../public/images/grad/grad-4.jpg';
+import grad5 from'../../public/images/grad/grad-5.jpg';
+import grad6 from'../../public/images/grad/grad-6.jpg';
+import grad7 from'../../public/images/grad/grad-7.jpg';
+import grad8 from'../../public/images/grad/grad-8.jpg';
+import grad9 from'../../public/images/grad/grad-9.jpg';
+import grad10 from'../../public/images/grad/grad-10.jpg';
+import grad11 from'../../public/images/grad/grad-11.jpg';
+import grad12 from'../../public/images/grad/grad-12.jpg';
+import grad13 from'../../public/images/grad/grad-13.jpg';
+import grad14 from'../../public/images/grad/grad-14.jpg';
+import grad15 from'../../public/images/grad/grad-15.jpg';
+import grad16 from'../../public/images/grad/grad-16.jpg';
+import grad17 from'../../public/images/grad/grad-17.jpg';
+import grad18 from'../../public/images/grad/grad-18.jpg';
+import grad19 from'../../public/images/grad/grad-19.jpg';
+import grad20 from'../../public/images/grad/grad-20.jpg';
+import grad21 from'../../public/images/grad/grad-21.jpg';
+
 const grads = [
     {
         id: 0,
-        src: '/../public/images/grad/grad-1.jpg'
+        src: grad1
     },
     {
         id: 1,
-        src: '/../public/images/grad/grad-2.jpg'
+        src: grad2
     },
     {
         id: 2,
-        src: '/../public/images/grad/grad-3.jpg'
+        src: grad3
     },
     {
         id: 3,
-        src: '/../public/images/grad/grad-4.jpg'
+        src: grad4
     },
     {
         id: 4,
-        src: '/../public/images/grad/grad-5.jpg'
+        src: grad5
     },
     {
         id: 5,
-        src: '/../public/images/grad/grad-6.jpg'
+        src: grad6
     },
     {
         id: 6,
-        src: '/../public/images/grad/grad-7.jpg'
+        src: grad7
     },
     {
         id: 7,
-        src: '/../public/images/grad/grad-8.jpg'
+        src: grad8
     },
     {
         id: 8,
-        src: '/../public/images/grad/grad-9.jpg'
+        src: grad9
     },
     {
         id: 9,
-        src: '/../public/images/grad/grad-10.jpg'
+        src: grad10
     },
     {
         id: 10,
-        src: '/../public/images/grad/grad-11.jpg'
+        src: grad11
     },
     {
         id: 11,
-        src: '/../public/images/grad/grad-12.jpg'
+        src: grad12
     },
     {
         id: 12,
-        src: '/../public/images/grad/grad-13.jpg'
+        src: grad13
     },
     {
         id: 13,
-        src: '/../public/images/grad/grad-14.jpg'
+        src: grad14
     },
     {
         id: 14,
-        src: '/../public/images/grad/grad-15.jpg'
+        src: grad15
     },
     {
         id: 15,
-        src: '/../public/images/grad/grad-16.jpg'
+        src: grad16
     },
     {
         id: 16,
-        src: '/../public/images/grad/grad-17.jpg'
+        src: grad17
     },
     {
         id: 17,
-        src: '/../public/images/grad/grad-18.jpg'
+        src: grad18
     },
     {
         id: 18,
-        src: '/../public/images/grad/grad-19.jpg'
+        src: grad19
     },
     {
         id: 19,
-        src: '/../public/images/grad/grad-20.jpg'
+        src: grad20
     },
     {
         id: 20,
-        src: '/../public/images/grad/grad-21.jpg'
+        src: grad21
     }
 ]
 
