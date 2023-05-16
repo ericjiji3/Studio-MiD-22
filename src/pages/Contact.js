243d8de978b4141797a5a3fe08ce3f20da4c5129
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2023-05-15 19:05:38 -0500

    added calendar npm

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,9 +5,10 @@ import { InlineWidget } from "react-calendly";
 import cover from '../../public/images/opening/opening-04.jpg';
 import Image from 'next/image';
 import { motion,AnimatePresence } from "framer-motion";
-import formpic from '../../public/images/creative/creative8.jpg';
+import formpic from '../../public/images/opening/opening-05.jpg';
 import { useState } from "react";
-import DatePicker from "react-multi-date-picker";
+import {DatePicker, Calendar} from "react-multi-date-picker";
+import DatePanel from "react-multi-date-picker/plugins/date_panel";
 
 export default function Contact(){
     const [value, setValue] = useState(new Date());
@@ -28,16 +29,28 @@ export default function Contact(){
                         <input className="text-input" autocomplete="name" placeholder="Email Address" type="text" name="your-email"/>
                         <input className="text-input" autocomplete="name" placeholder="Number" type="text" name="your-number"/>
                         <input className="text-input" autocomplete="name" placeholder="Project Type" type="text" name="your-project"/>
-                        <input className="text-input" autocomplete="name" placeholder="Availability" type="text" name="your-availability"/>
-                        <DatePicker value={value} onChange={setValue} multiple={true} />
+                        {/* <input className="text-input" autocomplete="name" placeholder="Availability" type="text" name="your-availability"/> */}
+                        {/* <DatePicker value={value} onChange={setValue} multiple={true} /> */}
+                        <h2 class='calendar-header'>Availability</h2>
+                        <Calendar 
+                        className="rmdp-mobile"
+                        value={value}
+                        onChange={setValue}
+                        multiple={true}
+                        plugins={[
+                            <DatePanel sort="date" />
+                          ]}
+                        />
                         <button className="submit-button" type="submit">Submit</button>
                     </div>
-                    {/* <Image src={formpic} 
-                        className='form-pic'
-                        alt='oops'
-                        width={100}
-
-                    /> */}
+                    <div className="form-pic-container">
+                        <Image src={formpic} 
+                            className='form-pic'
+                            alt='oops'
+                            width={500}
+                        />
+                    </div>
+                    
                 </form>
                 </motion.div>
             </div>  
